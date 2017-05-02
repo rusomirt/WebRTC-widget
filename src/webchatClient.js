126adae37c49dbebfddfb91f24868c4fbe56b793
Rusomirt <rusomirt@gmail.com>
2017-05-02 16:07:50 +0600

    End call screen: feedback review

--- src/webchatClient.js
+++ src/webchatClient.js
@@ -537,29 +537,29 @@ const Chat = (props) => {
                             Rate Luke's Lobster
                         </div>
                         <fieldset className={cn('rating')}>
-                            <input type="radio" id="star5" name="rating" value="5" />
-                            <label className={cn('rating__full-star')} htmlFor="star5" title="Awesome - 5 stars"></label>
-                            <input type="radio" id="star4half" name="rating" value="4 and a half" />
-                            <label className={cn('rating__half-star')} htmlFor="star4half" title="Pretty good - 4.5 stars"></label>
-                            <input type="radio" id="star4" name="rating" value="4" />
-                            <label className={cn('rating__full-star')} htmlFor="star4" title="Pretty good - 4 stars"></label>
-                            <input type="radio" id="star3half" name="rating" value="3 and a half" />
-                            <label className={cn('rating__half-star')} htmlFor="star3half" title="Meh - 3.5 stars"></label>
-                            <input type="radio" id="star3" name="rating" value="3" />
-                            <label className={cn('rating__full-star')} htmlFor="star3" title="Meh - 3 stars"></label>
-                            <input type="radio" id="star2half" name="rating" value="2 and a half" />
-                            <label className={cn('rating__half-star')} htmlFor="star2half" title="Kinda bad - 2.5 stars"></label>
-                            <input type="radio" id="star2" name="rating" value="2" />
-                            <label className={cn('rating__full-star')} htmlFor="star2" title="Kinda bad - 2 stars"></label>
-                            <input type="radio" id="star1half" name="rating" value="1 and a half" />
-                            <label className={cn('rating__half-star')} htmlFor="star1half" title="Meh - 1.5 stars"></label>
-                            <input type="radio" id="star1" name="rating" value="1" />
-                            <label className={cn('rating__full-star')} htmlFor="star1" title="Sucks big time - 1 star"></label>
-                            <input type="radio" id="starhalf" name="rating" value="half" />
-                            <label className={cn('rating__half-star')} htmlFor="starhalf" title="Sucks big time - 0.5 stars"></label>
+                            <input type='radio' id='star5' name='rating' value='5' />
+                            <label className={cn('rating__full-star')} htmlFor='star5' title='Awesome - 5 stars'></label>
+                            <input type='radio' id='star4half' name='rating' value='4 and a half' />
+                            <label className={cn('rating__half-star')} htmlFor='star4half' title='Pretty good - 4.5 stars'></label>
+                            <input type='radio' id='star4' name='rating' value='4' />
+                            <label className={cn('rating__full-star')} htmlFor='star4' title='Pretty good - 4 stars'></label>
+                            <input type='radio' id='star3half' name='rating' value='3 and a half' />
+                            <label className={cn('rating__half-star')} htmlFor='star3half' title='Meh - 3.5 stars'></label>
+                            <input type='radio' id='star3' name='rating' value='3' />
+                            <label className={cn('rating__full-star')} htmlFor='star3' title='Meh - 3 stars'></label>
+                            <input type='radio' id='star2half' name='rating' value='2 and a half' />
+                            <label className={cn('rating__half-star')} htmlFor='star2half' title='Kinda bad - 2.5 stars'></label>
+                            <input type='radio' id='star2' name='rating' value='2' />
+                            <label className={cn('rating__full-star')} htmlFor='star2' title='Kinda bad - 2 stars'></label>
+                            <input type='radio' id='star1half' name='rating' value='1 and a half' />
+                            <label className={cn('rating__half-star')} htmlFor='star1half' title='Meh - 1.5 stars'></label>
+                            <input type='radio' id='star1' name='rating' value='1' />
+                            <label className={cn('rating__full-star')} htmlFor='star1' title='Sucks big time - 1 star'></label>
+                            <input type='radio' id='starhalf' name='rating' value='half' />
+                            <label className={cn('rating__half-star')} htmlFor='starhalf' title='Sucks big time - 0.5 stars'></label>
                         </fieldset>
 
-                        <input className={cn('feedback__review')}/>
+                        <input className={cn('feedback__review')} placeholder='Write a review'/>
                     </div>
                 </div>;
     }
@@ -606,7 +606,7 @@ class Timer extends Component {
         );
     }
     format(n){
-        return (n > 9) ? ("" + n) : ("0" + n);
+        return (n > 9) ? ("" + n) : ('0' + n);
     }
     componentDidMount() {
         // Ticks every second
